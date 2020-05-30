491ec6fd2e24ebf50f516160c0b932e9b13d390f
Roshan <nahsorad@gmail.com>
2020-05-30 17:46:39 +0545

    Make post request at /execute

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 const socket = io.connect('http://localhost:3000');
+
+const codeEditor = document.getElementById('code-editor');
+const runBtn = document.getElementById('runBtn');
+const stdoutContainer = document.getElementById('stdout-container');
+
+runBtn.addEventListener('click', () => {
+    event.preventDefault();
+    const code = codeEditor.value;
+    const payload = {
+        "code": `${code}`,
+        "dockerConfig": "1"
+    }
+    if (code && code.trim() !== '') {
+        try {
+            fetch('http://localhost:3000/execute', {
+                method: 'POST',
+                body: JSON.stringify(payload),
+                headers: {
+                    'content-type': 'application/json'
+                }
+
+            });
+        } catch (err) {
+            console.err(err);
+        } 
+    }
+});
